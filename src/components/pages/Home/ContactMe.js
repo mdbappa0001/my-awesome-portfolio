07193f3b730117ff7d41a65dee49ba2d0282385f
Labib2003 <labib.salimi30@gmail.com>
2022-06-09 15:43:46 +0600

    social links added

--- src/components/pages/Home/ContactMe.js
+++ src/components/pages/Home/ContactMe.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const ContactMe = () => {
     return (
-        <div class="hero mb-32">
-            <div class="hero-content grid gap-10 grid-cols-1 lg:grid-cols-3">
-                <div class="text-center lg:text-left">
-                    <h1 class="text-5xl font-semibold mb-5">Get in touch</h1>
-                    <p class="text-xl leading-relaxed">Send me an email if you think I can help you out with any of your web related projects.</p>
+        <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: 'easeInOut' }}
+            className="mb-32"
+            class="hero mb-32"
+        >
+            <div class="hero-content grid gap-10 grid-cols-1 lg:grid-cols-5 p-0">
+                <div class="text-center lg:text-left col-span-2">
+                    <div className='mb-5'>
+                        <h1 class="text-5xl font-semibold mb-5">Get in touch</h1>
+                        <p class="text-xl leading-relaxed">Send me an email if you think I can help you out with any of your web related projects.</p>
+                    </div>
+                    <div>
+                        <h1 class="text-5xl font-semibold mb-5">My socials</h1>
+                        <div className='w-full grid grid-cols-3'>
+                            <a href="https://github.com/Labib2003"><img className='hover:scale-110' src="images/icons/github.svg" alt="my GitHub" /></a>
+                            <a href="https://www.linkedin.com/in/labib2003/"><img className='hover:scale-110' src="images/icons/linkedin.svg" alt="my LinkedIn" /></a>
+                            <a href="https://www.facebook.com/profile.php?id=100074531298092"><img className='hover:scale-110' src="images/icons/facebook.svg" alt="my Facebook" /></a>
+                        </div>
+                    </div>
                 </div>
-                <div class="card flex-shrink-0 w-full shadow-2xl bg-base-100 col-span-2">
+                <div class="card flex-shrink-0 w-full shadow-2xl bg-base-100 col-span-3">
                     <form
                         action="https://formsubmit.co/labib.salimi30@gmail.com"
                         method="POST"
@@ -48,12 +65,12 @@ const ContactMe = () => {
                             />
                         </div>
                         <div class="form-control mt-6">
-                            <input type="submit" class="btn btn-primary" value="Send Email"/>
+                            <input type="submit" class="btn btn-primary" value="Send Email" />
                         </div>
                     </form>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 };
 
